cf10f264c087b9f839f24a11ad4bdd4c4346c766
Kuntal-Shelke <156524938+Kuntal-Shelke@users.noreply.github.com>
2025-01-15 13:26:25 +0530

    check whether integer is min or max using pointer

---
+++ Maxmin.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, max;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    *max = (*a > *b) ? *a : *b;
+
+    printf("Maximum: %d\n", max);
+
+    return 0;
+}
